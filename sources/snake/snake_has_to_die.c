e0095a7ef94731781629e2945777eae25a7b1f03
Luca Ray <luca.ray@protonmail.ch>
2024-01-18 16:41:41 +0100

    add collisions with himeself

--- sources/snake/snake_has_to_die.c
+++ sources/snake/snake_has_to_die.c
@@ -1,6 +1,16 @@
 #include "../../includes/termin_snake.h"
 
+static int	is_outside(t_game *game);
+static int	is_eating_himself(t_game *game);
+
 int	snake_has_to_die(t_game *game)
+{
+	if (is_outside(game) || is_eating_himself(game))
+		return 1;
+	return 0;
+}
+
+static int	is_outside(t_game *game)
 {
 	int	x_max = getmaxx(game->win.game);
 	int	y_max = getmaxy(game->win.game);
@@ -10,4 +20,19 @@ int	snake_has_to_die(t_game *game)
 		return 1;
 	else
 		return 0;
+}
+
+static int	is_eating_himself(t_game *game)
+{
+	t_vec2	head_pos = {game->snake->pos.x, game->snake->pos.y};
+	t_snake	*temp = game->snake;
+
+	temp = temp->next;
+	while (temp != NULL)
+	{
+		if (head_pos.x == temp->pos.x && head_pos.y == temp->pos.y)
+			return 1;
+		temp = temp->next;
+	}
+	return 0;
 }
